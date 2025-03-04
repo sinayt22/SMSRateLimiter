6362acbea9e03746d61ec80856cbb84ee27bbc0e
Sinay Tarakanov <sinaytwork@gmail.com>
2025-03-04 11:24:56 -0500

    Add global exception and request logging middleware for improved error handling and logging

---
+++ SMSRateLimiter.API/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+using SMSRateLimiter.API.Models;
+
+namespace SMSRateLimiter.API.Middleware;
+
+
+public class GlobalExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+    public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;        
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unhandled exception occured.");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = new ErrorResponse{
+            Message = "An unexpected error occured"
+        };
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
