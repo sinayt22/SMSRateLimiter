c10e88e875d6ed0eceedb21821d63c3ed83cc8a1
Sinay Tarakanov <sinaytwork@gmail.com>
2025-03-04 11:48:55 -0500

    Add rate limiter configuration and middleware integration for enhanced request handling

--- SMSRateLimiter.API/Program.cs
+++ SMSRateLimiter.API/Program.cs
@@ -1,41 +1,35 @@
+
+using SMSRateLimiter.API.Middleware;
+using SMSRateLimiter.Core.Configuration;
+using SMSRateLimiter.Core.Interfaces;
+using SMSRateLimiter.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+var rateLimiterConfig = new RateLimiterConfig();
+builder.Configuration.GetSection("RateLimiter").Bind(rateLimiterConfig);
+
+builder.Services.AddSingleton(rateLimiterConfig);
+builder.Services.AddSingleton<ITokenBucketProvider, LocalTokenBucketProvider>();
+builder.Services.AddSingleton<IRateLimiterService, RateLimiterService>();
+builder.Services.AddHostedService(sp => (LocalTokenBucketProvider)sp.GetRequiredService<ITokenBucketProvider>());
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.UseMiddleware<GlobalExceptionMiddleware>();
+app.UseMiddleware<RequestLoggingMiddleware>();
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
